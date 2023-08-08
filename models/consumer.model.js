d15f8aa533136800a587d45693c3a4d9be2ce4c6
sabujmahonto <sabujmahonto@gmail.com>
2023-08-08 22:46:34 +0600

    consumer model done

---
+++ models/consumer.model.js
@@ -0,0 +1,31 @@
+const mongoose = require("mongoose");
+const consumerSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    image: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    consumptions: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Consumptions",
+      },
+    ],
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+module.exports = mongoose.model("Consumer", consumerSchema);
