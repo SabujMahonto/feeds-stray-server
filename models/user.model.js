f48f73bf70a0e41bb00593023af12f28d0b2abe1
sabujmahonto <sabujmahonto@gmail.com>
2023-08-08 22:07:40 +0600

    user model Schema done

---
+++ models/user.model.js
@@ -0,0 +1,52 @@
+const mongoose = require("mongoose");
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    address: {
+      type: String,
+      required: true,
+    },
+    occupation: {
+      type: String,
+      required: true,
+    },
+    provider: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Provider",
+      },
+    ],
+    consumer: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Consumer",
+      },
+    ],
+    review: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Review",
+      },
+    ],
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("User", userSchema);
