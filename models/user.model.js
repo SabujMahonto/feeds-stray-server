f23befc71aadd74d7266eb84cf5a8a1b425ed22b
sabujmahonto <sabujmahonto@gmail.com>
2023-08-11 07:27:41 +0600

    review routel done

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,7 @@
 const mongoose = require("mongoose");
+const validator = require("validator");
+const bcrypt = require("bcrypt");
+
 const userSchema = new mongoose.Schema(
   {
     name: {
@@ -8,6 +11,7 @@ const userSchema = new mongoose.Schema(
     email: {
       type: String,
       required: true,
+      unique: true,
     },
     image: {
       type: String,
@@ -29,24 +33,25 @@ const userSchema = new mongoose.Schema(
       type: String,
       required: true,
     },
-    roll: {
-      enum: ["user", "admin", "providerConnector", "consumerConnector"],
+    role: {
+      type: String,
+      enum: ["user", "providerConnector", "consumerConnector", "admin"],
       default: "user",
       required: true,
     },
-    provider: [
+    providers: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Provider",
       },
     ],
-    consumer: [
+    consumers: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Consumer",
       },
     ],
-    review: [
+    reviews: [
       {
         type: mongoose.Schema.Types.ObjectId,
         ref: "Review",
@@ -61,5 +66,87 @@ const userSchema = new mongoose.Schema(
     timestamps: true,
   }
 );
+
+userSchema.statics.signup = async function (
+  name,
+  email,
+  password,
+  image,
+  address,
+  occupation,
+  phoneNumber
+) {
+  //validate inputs
+  if (
+    !email ||
+    !password ||
+    !name ||
+    !image ||
+    !address ||
+    !occupation ||
+    !phoneNumber
+  ) {
+    throw new Error("all fields must be filled");
+  }
+
+  //validate email
+  if (!validator.isEmail(email)) {
+    throw new Error("Invalid email");
+  }
+
+  //validate password
+  if (!validator.isStrongPassword(password)) {
+    throw new Error(
+      "Password must contain 8+ characters, lowercase, uppercase, numeric and symbol"
+    );
+  }
+
+  //check if the user exists or not
+  const isExist = await this.findOne({ email });
+
+  if (isExist) {
+    throw new Error("User already exists");
+  }
+
+  //password encryption
+  const salt = await bcrypt.genSalt(10);
+  const hash = await bcrypt.hash(password, salt);
+
+  //create user
+  const user = await this.create({
+    name,
+    email,
+    password: hash,
+    image,
+    address,
+    occupation,
+    phoneNumber,
+  });
+
+  return user;
+};
+
+userSchema.statics.login = async function (email, password) {
+  //validate inputs
+  if (!email || !password) {
+    throw new Error("All fields must be filled");
+  }
+
+  //check if the user exists or not
+  const user = await this.findOne({ email }).populate("checkpost").exec();
+
+  if (!user) {
+    throw new Error("Incorrect email");
+  }
+
+  //password decryption
+  const match = await bcrypt.compare(password, user.password);
+
+  if (!match) {
+    throw new Error("Incorrect password");
+  }
+
+  return user;
+};
 
 module.exports = mongoose.model("User", userSchema);
